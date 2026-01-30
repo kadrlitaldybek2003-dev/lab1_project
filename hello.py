d4bc0b00b1fa30be7f346e785fcc63ef59c99240
kadrlitaldybek2003-dev <kadrlitaldybek2003@gmail.com>
2026-01-30 21:55:11 +0500

    зертхарна 2
    Signed-off-by: kadrlitaldybek2003-dev <kadrlitaldybek2003@gmail.com>

--- hello.py
+++ hello.py
@@ -1,9 +1,44 @@
-print("Hello, World!")
+a = int(input("a енгізіңіз: "))
+b = int(input("b енгізіңіз: "))
 
-name = input("Атыңыз кім? ")
-print(f"Қош келдіңіз, {name}!")
+print(a > b)
+print(a < b)
+print(a == b)
 
-a = int(input("Бірінші санды енгізіңіз: "))
-b = int(input("Екінші санды енгізіңіз: "))
 
-print(f"Екі санның қосындысы: {a + b}.")
+
+n = int(input("n енгізіңіз: "))
+
+if n % 2 == 0:
+    print("Жұп")
+else:
+    print("Тақ")
+
+
+x = int(input("x енгізіңіз: "))
+
+if 0 <= x <= 100:
+    print("Дұрыс диапазон")
+else:
+    print("Диапазоннан тыс")
+
+
+age = int(input("Жасыңызды енгізіңіз: "))
+
+if age >= 18 and age <= 60:
+    print("Жұмыс жасында")
+else:
+    print("Жұмыс жасында емес")
+
+
+    a = int(input("a бастапқы мәнін енгізіңіз: "))
+    a += 5
+    print(a)
+    a -= 2
+    print(a)
+    a *= 3
+    print(a)
+    a //= 2
+    print(a)
+
+
